6fb13d261e3e242fb464afd055dabf4182edb921
Zhen Liu <MeimiLiu@users.noreply.github.com>
2023-03-12 11:12:31 +1100

    Added Mac 3D asset in Hero Section #8

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,14 +5,14 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./macbook_pro_2021/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={1} groundColor="black" />
       <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
+        position={[-50, 50, 10]}
+        angle={0.5}
         penumbra={1}
         intensity={1}
         castShadow
@@ -21,9 +21,9 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.5 : 0.75}
-        position={isMobile ? [-1, -2, -2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 11 : 18}
+        position={isMobile ? [0, -2.5, 0] : [0, -3.5, 0]}
+        rotation={[0, 2.9, 0]}
       />
     </mesh>
   );
